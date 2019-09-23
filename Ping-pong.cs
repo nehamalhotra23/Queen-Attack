db4652031aaee978a3ff9bf7b0fb74286dface47
Ethan Samuels-Ellignson and Neha Malhotra <neha@>
2019-09-23 09:01:58 -0700

    complete ping pong project

--- Ping-pong.cs
+++ Ping-pong.cs
@@ -8,10 +8,16 @@ class Program
     string stringNumber = Console.ReadLine();
     int intNumber = int.Parse(stringNumber);
     // program code goes here
-    for (int index = 0; index <= intNumber; index++)
+    for (int index = 1; index <= intNumber; index++)
     {
       if(index % 3 == 0 && index % 5 == 0 ) {
         Console.WriteLine("ping pong");
+      } else if (index % 3 == 0) {
+        Console.WriteLine("ping");
+      } else if (index % 5 == 0) {
+        Console.WriteLine("pong");
+      } else {
+        Console.WriteLine(index);
       }
     }
   }
